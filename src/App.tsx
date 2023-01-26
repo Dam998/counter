f5368682a90ee2973409c9a6fd63f346adbc455b
Damiano <dpapi68@gmail.com>
2023-01-26 18:14:57 +0100

    feat(counter): create a counter + increment and reset buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+    const [count, setCount] = useState(0);
+
     return (
         <div className="App">
-            
+            <h2>
+                Counter: <span className='counter' data-content={count}>{count}</span>
+            </h2>
+            <div className='actionButtons'>
+                <button onClick={() => setCount(prev => ++prev)}>Increment</button>
+                <button onClick={() => setCount(0)}>Reset</button>
+            </div>
         </div>
     )
 }
